ea80290b3ac892c71c47cf1e6f9c664495dcffb8
John <john.wheeler.production.com>
2022-01-02 22:52:33 -0500

    more nitpicky styles, overriding some next defaults, specifically the border around the subscription form in the footer

--- components/footer.js
+++ components/footer.js
@@ -7,63 +7,68 @@ function Footer() {
 
     return (
         <footer className={styles.footer}>
-            <div className={styles.flexTop}>
-                <div className={styles.allLinksContainer}>
-                    <div className={styles.linksContainer}>
-                        <h2 className={styles.footerLabel}>Solutions</h2>
-                        <a href="" className={styles.footerLink}>Screen Sharing</a>
-                        <a href="" className={styles.footerLink}>Marketing</a>
-                        <a href="" className={styles.footerLink}>Commerce</a>
-                        <a href="" className={styles.footerLink}>Insights</a>
+            <div>
+                <div className={styles.flexTop}>
+                    <div className={styles.allLinksContainer}>
+                        <div className={styles.linksContainer}>
+                            <h2 className={styles.footerLabel}>Solutions</h2>
+                            <a href="" className={styles.footerLink}>Screen Sharing</a>
+                            <a href="" className={styles.footerLink}>Marketing</a>
+                            <a href="" className={styles.footerLink}>Commerce</a>
+                            <a href="" className={styles.footerLink}>Insights</a>
+                        </div>
+                        <div className={styles.linksContainer}>
+                            <h2 className={styles.footerLabel}>Resources</h2>
+                            <a href="" className={styles.footerLink}>Screen Sharing</a>
+                            <a href="" className={styles.footerLink}>Marketing</a>
+                            <a href="" className={styles.footerLink}>Commerce</a>
+                            <a href="" className={styles.footerLink}>Insights</a>
+                        </div>
+                        <div className={styles.linksContainer}>
+                            <h2 className={styles.footerLabel}>Company</h2>
+                            <a href="" className={styles.footerLink}>Screen Sharing</a>
+                            <a href="" className={styles.footerLink}>Marketing</a>
+                            <a href="" className={styles.footerLink}>Commerce</a>
+                            <a href="" className={styles.footerLink}>Insights</a>
+                        </div>
+                        <div className={styles.linksContainer}>
+                            <h2 className={styles.footerLabel}>Help</h2>
+                            <a href="" className={styles.footerLink}>Screen Sharing</a>
+                            <a href="" className={styles.footerLink}>Marketing</a>
+                            <a href="" className={styles.footerLink}>Commerce</a>
+                            <a href="" className={styles.footerLink}>Insights</a>
+                        </div>
                     </div>
-                    <div className={styles.linksContainer}>
-                        <h2 className={styles.footerLabel}>Resources</h2>
-                        <a href="" className={styles.footerLink}>Screen Sharing</a>
-                        <a href="" className={styles.footerLink}>Marketing</a>
-                        <a href="" className={styles.footerLink}>Commerce</a>
-                        <a href="" className={styles.footerLink}>Insights</a>
-                    </div>
-                    <div className={styles.linksContainer}>
-                        <h2 className={styles.footerLabel}>Company</h2>
-                        <a href="" className={styles.footerLink}>Screen Sharing</a>
-                        <a href="" className={styles.footerLink}>Marketing</a>
-                        <a href="" className={styles.footerLink}>Commerce</a>
-                        <a href="" className={styles.footerLink}>Insights</a>
-                    </div>
-                    <div className={styles.linksContainer}>
-                        <h2 className={styles.footerLabel}>Help</h2>
-                        <a href="" className={styles.footerLink}>Screen Sharing</a>
-                        <a href="" className={styles.footerLink}>Marketing</a>
-                        <a href="" className={styles.footerLink}>Commerce</a>
-                        <a href="" className={styles.footerLink}>Insights</a>
+                    <div className={styles.subscriptionContainer}>
+                        <h2 className={styles.footerLabel}>Subscribe to our newsletter</h2>
+                        <p className={styles.footerLink}>Get the latest news, articles, and resources, sent to your inbox every week.</p>
+                        <div>
+                            <form className={styles.form} action="" method="POST">
+                                <input className={styles.input} id="email" type="email" autoComplete="email" placeholder="Email" required />
+                                <button className={styles.btnSubscribe} type="submit">Subscribe</button>
+                            </form>
+                        </div>
                     </div>
                 </div>
-                <div className={styles.subscriptionContainer}>
-                    <h2 className={styles.footerLabel}>Subscribe to our newsletter</h2>
-                    <p className={styles.footerLink}>Get the latest news, articles, and resources, sent to your inbox every week.</p>
+                <div className={styles.flex}>
+                    <hr/>
+                </div>
+
+                <div className={styles.flex}>
                     <div>
-                        <form className={styles.form} action="" method="POST">
-                            <input className={styles.input} id="email" type="email" autoComplete="email" placeholder="Email" required />
-                            <button className={styles.btnMain} type="submit">Subscribe</button>
-                        </form>
+                        <p className={styles.copyright}>© {copyrightDate} Acme, Inc. All rights reserved.</p>
                     </div>
-                </div>
-            </div>
-            <hr/>
-            <div className={styles.flex}>
-                <div>
-                    <p className={styles.copyright}>© {copyrightDate} Acme, Inc. All rights reserved.</p>
-                </div>
-                <div className={styles.flexBottom}>
-                    <a href="" className={styles.iconContainer}>
-                        <Image src="/logo-facebook.svg" alt="Facebook Icon" width={20} height={20} />
-                    </a>
-                    <a href="" className={styles.iconContainer}>
-                        <Image src="/logo-twitter.svg" alt="Facebook Icon" width={20} height={20} />
-                    </a>
-                    <a href="" className={styles.iconContainerRight}>
-                        <Image src="/logo-instagram.svg" alt="Facebook Icon" width={20} height={20} />
-                    </a>
+                    <div className={styles.flexBottom}>
+                        <a href="" className={styles.iconContainer}>
+                            <Image src="/logo-facebook.svg" alt="Facebook Icon" width={20} height={20} />
+                        </a>
+                        <a href="" className={styles.iconContainer}>
+                            <Image src="/logo-twitter.svg" alt="Facebook Icon" width={20} height={20} />
+                        </a>
+                        <a href="" className={styles.iconContainerRight}>
+                            <Image src="/logo-instagram.svg" alt="Facebook Icon" width={20} height={20} />
+                        </a>
+                    </div>
                 </div>
             </div>
         </footer>
